98e5bb829f4f4492505d7502d48ccacbc1ac02f3
Pedro-Benevides <pvsbenevides197@gmail.com>
2022-07-24 15:14:20 -0300

    feat(app.module): configura conexao do typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import 'dotenv/config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as any,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT as unknown as number,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
