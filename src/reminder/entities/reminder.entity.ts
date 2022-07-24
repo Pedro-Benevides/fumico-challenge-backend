df1ca45404ffb1b6f88501fef672823224f7cee0
Pedro-Benevides <pvsbenevides197@gmail.com>
2022-07-24 15:35:06 -0300

    feat(entities/reminder.entity): implementa entity

--- src/reminder/entities/reminder.entity.ts
+++ src/reminder/entities/reminder.entity.ts
@@ -1,1 +1,31 @@
-export class Reminder {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Reminder {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  userId: number;
+
+  @Column()
+  title: string;
+
+  @Column('text')
+  description: string;
+
+  @Column({ default: false })
+  status: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
