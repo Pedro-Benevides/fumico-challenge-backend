68ec56d115e33ea78325d5a7605953cc4137eda0
Pedro-Benevides <pvsbenevides197@gmail.com>
2022-07-24 22:46:36 -0300

    feat(user.service): implementa escopo de consulta por email

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  async findByEmail(email: string): Promise<User> {
+    return await this.userRepository.findOneByOrFail({ email });
+  }
+}
