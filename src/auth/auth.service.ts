82af35e18fd532c3238f6f468cec92874ae8d630
Pedro-Benevides <pvsbenevides197@gmail.com>
2022-07-24 22:48:43 -0300

    feat(authService): implementa service de auth com método de validação

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UserService) {}
+
+  async validateUser(email: string, password: string) {
+    const user = await this.userService.findByEmail(email);
+    const match = await bcrypt.compare(password, user.password);
+
+    if (user && match) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+}
