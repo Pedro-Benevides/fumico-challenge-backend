1bf2a8cda3605a5cb81f555d0c7bc65753f2487f
Pedro-Benevides <pvsbenevides197@gmail.com>
2022-07-25 23:44:02 -0300

    feat(auth.service): implementa método de login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
-import bcrypt from 'bcrypt';
+import { compareSync } from 'bcrypt';
+import { User } from 'src/user/entities/user.entity';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async validateUser(email: string, password: string) {
     const user = await this.userService.findByEmail(email);
-    const match = await bcrypt.compare(password, user.password);
+    const match = compareSync(password, user.password);
 
     if (user && match) {
       const { password, ...result } = user;
       return result;
     }
     return null;
+  }
+
+  async login(user: User): Promise<any> {
+    const payload = { sub: user.id, email: user.email };
+
+    return {
+      user: user,
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
