36397f8ed4aa6e1410c1376d3e23b048e85e2a47
Pedro-Benevides <pvsbenevides197@gmail.com>
2022-07-27 20:37:38 -0300

    feat(user.service): implementa métodos de CRUD

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -11,5 +13,25 @@ export class UserService {
 
   async findByEmail(email: string): Promise<User> {
     return await this.userRepository.findOneByOrFail({ email });
+  }
+
+  async create(createUserDto: CreateUserDto) {
+    await this.userRepository.save(createUserDto);
+  }
+
+  async findAll(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+
+  async findOne(id: number): Promise<User> {
+    return this.userRepository.findOneByOrFail({ id });
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto): Promise<void> {
+    await this.userRepository.update(id, updateUserDto);
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.userRepository.delete(id);
   }
 }
