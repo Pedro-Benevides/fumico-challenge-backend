4a3810be281bc144bf66bbd21733c1038c9b87b5
Pedro-Benevides <pvsbenevides197@gmail.com>
2022-07-29 14:35:22 -0300

    feat(app.controller.spec): implementa teste unitario

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,22 +1,80 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { LocalAuthGuard } from './auth/guards/local-auth.guard';
+
+const validatedUser = {
+  user: {
+    id: 1,
+    email: 'teste@mail.com',
+  },
+  access_token: '',
+};
 
 describe('AppController', () => {
   let appController: AppController;
+  let authService: AuthService;
 
   beforeEach(async () => {
+    const mockLocalGuard = {};
+
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
-    }).compile();
+      providers: [
+        AppService,
+        {
+          provide: AuthService,
+          useValue: {
+            login: jest.fn().mockResolvedValue(validatedUser),
+          },
+        },
+      ],
+    })
+      .overrideGuard(LocalAuthGuard)
+      .useValue(mockLocalGuard)
+      .compile();
 
     appController = app.get<AppController>(AppController);
+    authService = app.get<AuthService>(AuthService);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.login()).toBe('Hello World!');
+  it('should be defined', () => {
+    expect(AppController).toBeDefined();
+    expect(AuthService).toBeDefined();
+  });
+
+  describe('login', () => {
+    it('should return a autenticated User', async () => {
+      //Arrange
+      const req = {
+        user: {
+          email: 'user@example.com',
+          password: 'password',
+        },
+      };
+      //Act
+      const result = await appController.login(req);
+
+      //Assert
+      expect(result).toEqual(validatedUser);
+      expect(authService.login).toHaveBeenCalledWith(req.user);
+      expect(authService.login).toHaveBeenCalledTimes(1);
+    });
+
+    it('should throw an exeption', () => {
+      //Arrange
+      const req = {
+        user: {
+          email: 'user@example.com',
+          password: 'password',
+        },
+      };
+
+      jest.spyOn(authService, 'login').mockRejectedValueOnce(new Error());
+
+      //Assert
+      expect(appController.login(req)).rejects.toThrowError();
     });
   });
 });
